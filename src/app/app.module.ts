c0276d8671b33a1c2dd9135a7219d4ff265cb5d2
Pravin <vkvk3113@gmail.com>
2024-10-15 08:00:42 +0530

    added all components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { QuizListComponent } from './components/quiz-list/quiz-list.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { CreateQuizComponent } from './components/create-quiz/create-quiz.component';
+import { QuestionComponent } from './components/question/question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    QuizListComponent,
+    DashboardComponent,
+    QuizComponent,
+    CreateQuizComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
